19e643cf6651f5e3e9993a3df01d1ec56c8651a9
root <root@Xrtls>
2023-07-28 16:06:37 +0300

    Process and PID file

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,38 @@
+#include <stdio.h> /* printf */
+#include <unistd.h> /* sleep */
+#include <stdlib.h> /* exit */
+
+/**
+* infinite_while - create infinite
+*  sleep loop
+*  Run an infinite while loop
+* Return: 0
+*/
+int infinite_while(void)
+{
+while (1)
+{
+sleep(1);
+}
+return (0);
+}
+/**
+* main - create 5 zombie processes
+* *********
+* Return: infinite_while zombies
+*/
+int main(void)
+{
+pid_t zombiePID;
+unsigned int i;
+
+for (i = 0; i < 5; i++)
+{
+zombiePID = fork();
+if (zombiePID == 0)
+exit(0);
+else
+printf("Zombie process created, PID: %d\n", zombiePID);
+}
+return (infinite_while());
+}
